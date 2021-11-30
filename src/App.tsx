0c3a1076dbf1a310b4a4c9d575bc9148ee037d18
Alex Grobovsky <ktotaam@mail.ru>
2021-11-30 10:40:26 +0500

    feat: add app ColorModeSwitcher

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import * as React from "react"
 import {ChakraProvider, theme,} from "@chakra-ui/react"
+import {Portal} from "./components/ui";
+import {ColorModeSwitcher} from "./components";
 
 
 export const App = () => (
-  <ChakraProvider theme={theme}>
-    Hello
-  </ChakraProvider>
+    <ChakraProvider theme={theme}>
+        Hello
+        <Portal>
+            <ColorModeSwitcher position="fixed" right="40px" top="50%"/>
+        </Portal>
+    </ChakraProvider>
 )
