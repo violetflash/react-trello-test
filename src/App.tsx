340158728e97abe54fdd4a175d4bd94c489b115f
Alex Grobovsky <ktotaam@mail.ru>
2021-11-30 10:26:53 +0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import * as React from "react"
+import {ChakraProvider, theme,} from "@chakra-ui/react"
+
+
+export const App = () => (
+  <ChakraProvider theme={theme}>
+    Hello
+  </ChakraProvider>
+)
