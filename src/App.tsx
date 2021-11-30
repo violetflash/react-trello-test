0f30e5b2d0d2b5f0488685bb961441439b464f4a
Alex Grobovsky <ktotaam@mail.ru>
2021-11-30 13:13:10 +0500

    feat add default opened modal with username request

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,53 @@
-import * as React from "react"
-import {ChakraProvider, theme,} from "@chakra-ui/react"
-import {Portal} from "./components/ui";
+import React from "react";
+import {
+    ChakraProvider,
+    Icon,
+    Input,
+    InputGroup,
+    InputLeftElement,
+    theme,
+    useDisclosure
+} from "@chakra-ui/react"
+import {ModalCasing, Portal} from "./components/ui";
 import {ColorModeSwitcher} from "./components";
+import {useCallback, useState} from "react";
+import {FaUser} from "react-icons/all";
 
-export const App = () => (
-    <ChakraProvider theme={theme}>
-        Hello
-        <Portal>
-            <ColorModeSwitcher position="fixed" right="40px" top="50%"/>
-        </Portal>
-    </ChakraProvider>
-)
+export const App = () => {
+    const {isOpen, onClose} = useDisclosure({defaultIsOpen: true});
+    const [name, setName] = useState<string>("");
+
+    const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setName(event.target.value);
+    }
+
+    const getName = useCallback(() => {
+        console.log(name);
+        onClose();
+    }, [onClose, name]);
+
+    return (
+        <ChakraProvider theme={theme}>
+            Hello
+
+            <ModalCasing
+                isOpen={isOpen}
+                onClose={onClose}
+                modalTitle="Введите свое имя:"
+                onAction={getName}
+                actionTitle="Сохранить"
+            >
+                <InputGroup>
+                    <InputLeftElement
+                        pointerEvents='none'
+                        children={<Icon as={FaUser} color='gray.300'/>}
+                    />
+                    <Input type='text' placeholder='Имя пользователя' onChange={handleInput}/>
+                </InputGroup>
+            </ModalCasing>
+            <Portal>
+                <ColorModeSwitcher position="fixed" right="40px" top="50%"/>
+            </Portal>
+        </ChakraProvider>
+    );
+}
--- src/components/ui/ModalCasing/ModalCasing.tsx
+++ src/components/ui/ModalCasing/ModalCasing.tsx
@@ -10,11 +10,12 @@ import {
 import {FC} from "react";
 
 interface IModalProps {
-    children: React.ReactNode;
+    children?: React.ReactNode;
     isOpen: boolean;
     onAction?: () => void;
     actionTitle?: string;
     onClose: () => void;
+    modalTitle: string;
 }
 
 export const ModalCasing: FC<IModalProps> = (
@@ -23,12 +24,13 @@ export const ModalCasing: FC<IModalProps> = (
         isOpen,
         onAction,
         actionTitle,
+        modalTitle,
         onClose
     }) => {
 
     const secondaryButton =
         onAction ?
-            <Button variant='ghost' onClick={onAction}>{actionTitle}</Button> :
+            <Button colorScheme="blue" onClick={onAction}>{actionTitle}</Button> :
             null
     ;
 
@@ -36,17 +38,13 @@ export const ModalCasing: FC<IModalProps> = (
         <Modal isOpen={isOpen} onClose={onClose}>
             <ModalOverlay />
             <ModalContent>
-                <ModalHeader>Modal Title</ModalHeader>
+                <ModalHeader mt="15px">{modalTitle}</ModalHeader>
                 <ModalCloseButton />
                 <ModalBody>
-                    Содержимое попапа
                     {children}
                 </ModalBody>
 
                 <ModalFooter>
-                    <Button colorScheme='blue' mr={3} onClick={onClose}>
-                        Ок
-                    </Button>
                     {secondaryButton}
                 </ModalFooter>
             </ModalContent>
